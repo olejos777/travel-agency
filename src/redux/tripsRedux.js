1a59cd9929b043784a4fb6c8b172668558fa2e2e
olejos777 <olejniczakk.maciejj@gmail.com>
2020-06-08 19:26:24 +0200

    add filtering by duration and tag

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,41 +1,50 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
 
   // filter by search phrase
-  if(filters.searchPhrase){
+  if (filters.searchPhrase) {
     const pattern = new RegExp(filters.searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
 
   // TODO - filter by duration
+  output = output.filter(trip =>
+    (trip.days >= filters.duration.from &&
+      trip.days <= filters.duration.to));
 
   // TODO - filter by tags
+  if (filters.tags.length > 0) {
+    filters.tags.forEach(tag =>
+      output = output.filter(trip =>
+        trip.tags.includes(tag) ? output : null)
+    );
+  }
 
   // TODO - sort by cost descending (most expensive goes first)
 
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
+export const getTripById = ({ trips }, tripId) => {
   const filtered = trips;
 
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
+export const getTripsForCountry = ({ trips }, countryCode) => {
   const filtered = trips;
 
   // TODO - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
